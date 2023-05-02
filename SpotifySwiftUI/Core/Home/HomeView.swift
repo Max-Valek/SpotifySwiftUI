390676329c05e7feaed0a5c8d2957727b955476d
Max Valek <valek.maxwell@gmail.com>
2023-05-02 09:49:46 -0500

    add color theme

--- SpotifySwiftUI/Core/Home/HomeView.swift
+++ SpotifySwiftUI/Core/Home/HomeView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct HomeView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            Color.theme.background
+                .ignoresSafeArea()
+        }
     }
 }
 
---
+++ SpotifySwiftUI/Extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  SpotifySwiftUI
+//
+//  Created by Max Valek on 5/2/23.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static var theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let background = Color("background")
+    let text = Color("text")
+    let lightGreen = Color("light-green")
+    let darkGreen = Color("dark-green")
+}
