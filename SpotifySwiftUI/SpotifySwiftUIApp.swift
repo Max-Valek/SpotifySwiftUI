ca3235d220c03a5eb60f25c9dbd170f0142259b2
Max Valek <valek.maxwell@gmail.com>
2023-05-02 09:36:34 -0500

    Initial Commit

---
+++ SpotifySwiftUI/SpotifySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifySwiftUIApp.swift
+//  SpotifySwiftUI
+//
+//  Created by Max Valek on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotifySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
