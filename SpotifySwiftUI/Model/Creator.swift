fecfc550bdc115d900173ffe434aa870813087b5
Max Valek <valek.maxwell@gmail.com>
2023-05-03 12:34:47 -0500

    create simple creator model

---
+++ SpotifySwiftUI/Model/Creator.swift
@@ -0,0 +1,15 @@
+//
+//  Creator.swift
+//  SpotifySwiftUI
+//
+//  Created by Max Valek on 5/3/23.
+//
+
+import Foundation
+
+struct Creator {
+    var name: String
+    var songs: [String] = []
+    var shows: [String] = []
+    var monthlyListeners: Int = 0
+}
