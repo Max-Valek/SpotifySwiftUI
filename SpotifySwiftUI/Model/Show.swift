9e16beb3688cb6e0af5d00c5a76ae69c9a379abd
Max Valek <valek.maxwell@gmail.com>
2023-05-03 12:37:28 -0500

    create simple show model

---
+++ SpotifySwiftUI/Model/Show.swift
@@ -0,0 +1,14 @@
+//
+//  Show.swift
+//  SpotifySwiftUI
+//
+//  Created by Max Valek on 5/3/23.
+//
+
+import Foundation
+
+struct Show {
+    var name: String
+    var creator: String
+    var episodes: [String] = []
+}
